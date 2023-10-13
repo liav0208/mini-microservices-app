e9f4db7a3ae048ba59bea8c42316717d7a4d9e1c
liav <liav0208@gmail.com>
2023-10-13 15:56:00 +0300

    last fix

--- posts/index.js
+++ posts/index.js
@@ -13,7 +13,7 @@ app.get("/posts", (_, res) => {
   res.send(posts);
 });
 
-app.post("/posts/create", async (_, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
 
   const { title } = req.body;
